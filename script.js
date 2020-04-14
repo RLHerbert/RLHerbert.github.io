5c5b426ce20439e65c83264dec1447ab578a68ee
RhymesWithCow <37909949+RhymesWithCow@users.noreply.github.com>
2020-04-13 22:14:53 -0700

    Toggleable dark mode partially implemented

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function removeTransition() {
     var body = document.getElementById("body");
     body.classList.remove("preload");
 }
+
+function darkModeToggle(){
+    document.documentElement.style.setProperty('--page-body-background','var(--solarized-base03)')
+}
